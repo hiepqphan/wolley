76bb8f271df19896cb69bc515276e0bc17390c91
Hiep Phan <hiepqphan@hotmail.com>
2019-05-07 13:02:32 -0400

    Added Ball Spawner

---
+++ Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 5f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(ball, new Vector2(-10, 10), Quaternion.identity);
+    }
+}
